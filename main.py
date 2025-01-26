883166048adb5f5f7d49c4605a34ffe6b3338dd1
costmaster1 <osoben1@yandex.ru>
2025-01-26 21:35:15 +0500

    Save 26.01.2025

---
+++ main.py
@@ -0,0 +1,60 @@
+# Импортируем модули
+import requests
+from bs4 import BeautifulSoup
+
+# Имитируем подключение через браузер Mozilla на Windows
+st_useragent = "Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0"
+st_accept = "text/html"
+# формируем хеш заголовков
+headers = {
+   "Accept": st_accept,
+   "User-Agent": st_useragent
+}
+
+for count in range(1, 3):
+ # Создаем сессию
+ url = f"https://habr.com/ru/flows/develop/articles/page{count}"
+
+# Отправим GET()-запрос на сайт и сохраним полученное в переменную 'page':
+page = requests.get(url, headers)
+
+# Проверим статус запроса
+print("Статус запроса =", page.status_code)
+
+# Создаем объект BeautifulSoup c параметром 'lxml'
+soup = BeautifulSoup(page.text, 'lxml')
+
+# Находим все статьи с классом "tm-articles-list__item" и сохраняем их в переменной articles
+articles = soup.find_all("article", class_="tm-articles-list__item")
+
+
+
+
+ # Выводим заголовки статей и ссылки на них:
+for article in articles:
+   title_articles = article.find("h2", class_="tm-title tm-title_h2")
+   url_articles = "https://habr.com" + article.find("a", class_="tm-title__link").get("href")
+   print(title_articles.text + "\n" + url_articles + "\n")
+
+
+   
+
+
+   
+   
+   
+    
+
+
+    
+
+
+
+
+ 
+
+
+
+
+
+  
