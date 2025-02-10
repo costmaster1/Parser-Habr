d954b267bfaf2bf40c9c177fc856fe28c89674ca
costmaster1 <osoben1@yandex.ru>
2025-02-10 13:41:06 +0500

    Save 10.02.2025

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ headers = {
    "User-Agent": st_useragent
 }
 
-for count in range(1, 5):
+for count in range(1, 2):
  sleep(1)
  # Создаем сессию
  url = f"https://habr.com/ru/flows/develop/articles/page{count}"
@@ -58,13 +58,28 @@ for url in list_artikles_url:
         # Выводим ссылку на картинку статьи
         print(img_articles_url + "\n")
 
+# Объявляем блок парсинга статьи
+articles = soup.find("div", class_="article-formatted-body article-formatted-body article-formatted-body_version-2")
+# Объявляем функцию рекурсивного опроса текста статьи
+def parse_articles(articles, level=1):
+    output = ""
+    for elem in articles.find_all(['p', 'h2', 'h3', 'h4' ], recursive=False):
+        # Обработка заголовков параграфов
+        if elem.name.startswith('h'):
+            output += f"{'#' * level} {elem.get_text()}\n"
+        elif elem.name == 'p':
+            output += f"{elem.get_text()}\n\n"
+
+parsed_articles = articles(soup.articles)
+print(parsed_articles)
+
     # Пауза для демонстрации скорости обработки
-    sleep(1)
+sleep(1)
     # Проверяем статус запроса
-    print("Статус запроса =", page.status_code)
-    print("-------------------------------------------")
+print("Статус запроса =", page.status_code)
+print("-------------------------------------------")
     # Пауза для демонстрации скорости обработки
-    sleep(2)
+sleep(2)
 
      
 
